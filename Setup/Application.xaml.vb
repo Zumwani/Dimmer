b4bafcc6b17bbffc44bf8f581d5c5818d000cc83
Andreas Ingeholm <andreas.ingeholm@gmail.com>
2017-12-17 17:44:02 +0100

    Revert "Revert "Added Contents.zip to gitignore.""
    This reverts commit 780e4c83ae4730f4c51c7d50de2301ce2156a887.

--- Setup/Application.xaml.vb
+++ Setup/Application.xaml.vb
@@ -1,6 +1,29 @@
 ﻿Class Application
 
-    ' Application-level events, such as Startup, Exit, and DispatcherUnhandledException
-    ' can be handled in this file.
+    Public Shared ReadOnly Property IsInstalling As Boolean = True
+    Public Shared ReadOnly Property IsUninstalling As Boolean
+    Public Shared ReadOnly Property InstalledGUID As String
+
+    Protected Overrides Sub OnStartup(e As StartupEventArgs)
+
+        For Each arg In e.Args
+            If arg.StartsWith("-uninstall:") Then
+
+                _IsInstalling = False
+                _IsUninstalling = True
+
+                Dim guid = arg.Substring("-uninstall:".Length)
+
+                If Not System.Guid.TryParse(guid, New Guid) Then
+                    MessageBox.Show("-uninstall was passed as an argument, but the specified guid was invalid. The setup will now close.")
+                    Shutdown()
+                End If
+
+                _InstalledGUID = guid
+
+            End If
+        Next
+
+    End Sub
 
 End Class
--- Setup/Commands/StartUninstallCommand.vb
+++ Setup/Commands/StartUninstallCommand.vb
@@ -1,28 +1,16 @@
 ﻿Namespace Commands
 
-    Public Class CloseSetupAndOpenAppCommand
+    Public Class StartUninstallCommand
         Inherits Markup.MarkupExtension
         Implements ICommand
 
         Public Event CanExecuteChanged As EventHandler Implements ICommand.CanExecuteChanged
 
         Public Sub Execute(parameter As Object) Implements ICommand.Execute
-
-            Dim path = Setup.Current.Path + "\Dimmer.exe"
-            If IO.File.Exists(path) Then
-                Process.Start(path, "-config")
-            End If
-
-            Windows.Application.Current.Shutdown()
-
+            Setup.Current.Uninstall()
         End Sub
 
         Public Function CanExecute(parameter As Object) As Boolean Implements ICommand.CanExecute
-
-            'Dim isSelected = TryCast(TryCast(parameter, RoutedEventArgs)?.OriginalSource, TabItem)?.IsSelected
-            'Dim isInstalled = (IO.File.Exists(Setup.Current.MainExecutable))
-
-            'Return (isSelected And isInstalled)
             Return True
         End Function
 
