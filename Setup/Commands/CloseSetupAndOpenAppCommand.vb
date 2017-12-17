780e4c83ae4730f4c51c7d50de2301ce2156a887
Andreas Ingeholm <andreas.ingeholm@gmail.com>
2017-12-17 17:42:26 +0100

    Revert "Added Contents.zip to gitignore."
    This reverts commit 3393295e66fe875f6aa7b582470ae9860a61222b.

--- Setup/Commands/CloseSetupAndOpenAppCommand.vb
+++ Setup/Commands/CloseSetupAndOpenAppCommand.vb
@@ -9,21 +9,18 @@
         Public Sub Execute(parameter As Object) Implements ICommand.Execute
 
             Dim path = Setup.Current.Path + "\Dimmer.exe"
-            If IO.File.Exists(path) Then
-                Process.Start(path, "-config")
-            End If
-
+            Process.Start(path, "-config")
             Windows.Application.Current.Shutdown()
 
         End Sub
 
         Public Function CanExecute(parameter As Object) As Boolean Implements ICommand.CanExecute
 
-            'Dim isSelected = TryCast(TryCast(parameter, RoutedEventArgs)?.OriginalSource, TabItem)?.IsSelected
-            'Dim isInstalled = (IO.File.Exists(Setup.Current.MainExecutable))
+            Dim isSelected = TryCast(TryCast(parameter, RoutedEventArgs)?.OriginalSource, TabItem)?.IsSelected
+            Dim isInstalled = (IO.File.Exists(Setup.Current.MainExecutable))
 
-            'Return (isSelected And isInstalled)
-            Return True
+            Return (isSelected And isInstalled)
+
         End Function
 
         Public Overrides Function ProvideValue(serviceProvider As IServiceProvider) As Object
